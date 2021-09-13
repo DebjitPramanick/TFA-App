b30132ac1ad230bdb5538d64b0d81cdb96a6a111
DebjitPramanick <dpramanick9990@gmail.com>
2021-09-13 09:01:42 +0530

    UI Changes

--- src/TFA.js
+++ src/TFA.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const TFA = () => {
+
+    const [number, setNumber] = useState(null)
+    const [code, setCode] = useState(null)
+
+    const sendCode = () => {
+
+    }
+
     return (
         <div className="container">
             <div className="box">
+                <h2 style={{textAlign: 'center', marginBottom: '20px'}}>2 FA</h2>
                 <h3>Enter Mobile No.</h3>
-                <input type="text" />
-                <button>Verify</button>
+                <div className="flex">
+                    <input type="text" value={number}
+                    onChange={(e) => setNumber(e.target.value)} />
+                    <button className="send" onClick={sendCode}>Send OTP</button>
+                </div>
+
+                <h3 className="second">Enter OTP</h3>
+                <div className="flex">
+                    <input type="text" value={code}
+                    onChange={(e) => setCode(e.target.value)}/>
+                    <button className="send">Verify</button>
+                </div>
             </div>
         </div>
     )
