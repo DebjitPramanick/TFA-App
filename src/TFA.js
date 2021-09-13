c4f17fb7a4c3dacfc1ae6c6e0fa7a26f6148e2fd
DebjitPramanick <dpramanick9990@gmail.com>
2021-09-13 08:26:38 +0530

    Init

---
+++ src/TFA.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TFA = () => {
+    return (
+        <div className="container">
+            <div className="box">
+                <h3>Enter Mobile No.</h3>
+                <input type="text" />
+                <button>Verify</button>
+            </div>
+        </div>
+    )
+}
+
+export default TFA
